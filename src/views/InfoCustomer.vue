712a359a317a2d5d7edb61b8e769a102bd596b23
Thong Nguyen <nlcthong1997@gmail.com>
2020-04-26 20:57:00 +0700

    update view info customer

--- src/views/InfoCustomer.vue
+++ src/views/InfoCustomer.vue
@@ -1,52 +1,69 @@
 <template>
   <div>
     <b-card title="Thông tin tài khoản">
-      <div class="info">
-        Kính chào quý khách
-        <br />
-        <br />
-      </div>
-      <div class="content">
-        <b-table hover :items="items"></b-table>
-      </div>
+      <hr />
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="8">
+          <div class="line-bar"></div>
+
+          <b-form-group id label-cols-sm="4" label-cols-lg="3" label="Tên tài khoản">
+            <b-form-input id="fullName" :value="infoCustomer.fullName" readonly="readonly"></b-form-input>
+          </b-form-group>
+
+          <b-form-group id label-cols-sm="4" label-cols-lg="3" label="Số tài khoản">
+            <b-form-select v-model="selected" :options="options"></b-form-select>
+          </b-form-group>
+
+          <b-form-group id label-cols-sm="4" label-cols-lg="3" label="Số dư hiện tại">
+            <b-row>
+              <b-col cols="9">
+                <b-form-input id="fullName" :value="selected.amount" readonly="readonly"></b-form-input>
+              </b-col>
+              <b-col>
+                <b-form-input id="currency" :value="selected.currency" readonly="readonly"></b-form-input>
+              </b-col>
+            </b-row>
+          </b-form-group>
+          
+        </b-col>
+        <b-col></b-col>
+      </b-row>
     </b-card>
   </div>
 </template>
 
 <script>
 import { fetch } from "../utils/apiCaller";
-// import * as axios from "axios";
+
 export default {
   props: {
     loggedLv3: { type: Boolean, default: false }
   },
   data() {
     return {
-      items: [
-        { "Thông tin": "Tên tài khoản", "Nội dung": "" },
-        { "Thông tin": "Số tài khoản", "Nội dung": "" },
-        { "Thông tin": "Số dư hiện tại", "Nội dung": "" },
-        { "Thông tin": "Loại tiền tệ", "Nội dung": "" }
-      ],
-      infoCustomer: [],
+      infoCustomer: {},
+      listCard: [],
       isTokenExpired: false,
-      isLoadding: false
+      isLoadding: false,
+      options: [],
+      selected: null
     };
   },
   created() {
     this.fetchData();
   },
   watch: {
-    isTokenExpired: async function (isExpired) {
+    isTokenExpired: async function(isExpired) {
       if (isExpired) {
-        const tokenStorage = JSON.parse(localStorage.getItem("token"))
+        const tokenStorage = JSON.parse(localStorage.getItem("token"));
         await this.getAndSetNewToken(tokenStorage);
         await this.fetchData();
       }
     },
-    isLoadding: function (isLoad) {
+    isLoadding: function(isLoad) {
       if (isLoad) {
-        this.appendData(this.infoCustomer);
+        this.appendListCard(this.listCard);
         this.isLoadding = false;
       }
     }
@@ -62,8 +79,8 @@ export default {
 
       fetch("/info-customer", "GET")
         .then(res => {
-          console.log(res.data.customer);
           this.infoCustomer = res.data.customer;
+          this.listCard = res.data.listCard;
           this.isTokenExpired = false;
           this.isLoadding = true;
         })
@@ -71,14 +88,22 @@ export default {
           if (err.status == 401) {
             this.isTokenExpired = true;
           }
-        })
+        });
     },
 
-    appendData(data) {
-      this.items[0]["Nội dung"] = data.fullName;
-      // this.items[1]["Nội dung"] = data.cardNumber;
-      // this.items[2]["Nội dung"] = data.currentAmount;
-      // this.items[3]["Nội dung"] = data.currency;
+    appendListCard(listCard) {
+      listCard.map(card => {
+        if (card.type == "pay") {
+          this.selected = {
+            amount: card.currentAmount,
+            currency: card.currency
+          };
+        }
+        this.options.push({
+          value: { amount: card.currentAmount, currency: card.currency },
+          text: card.cardNumber
+        });
+      });
     },
 
     async getAndSetNewToken(token) {
@@ -88,12 +113,12 @@ export default {
           localStorage.setItem("token", JSON.stringify(res.data));
         })
         .catch(err => {
-          if (err.data === 'jwt expired' || err.status == 400) {
+          if (err.data === "jwt expired" || err.status == 400) {
             isError = true;
           }
-          console.error('Get new token failed: ', err.data);
+          console.error("Get new token failed: ", err.data);
         });
-      
+
       if (isError) {
         this.removeClientData();
       }
@@ -109,4 +134,7 @@ export default {
 </script>
 
 <style>
+.line-bar {
+  margin-top: 1em;
+}
 </style>
